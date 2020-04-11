1cad364930ba31f421b9aaec2896ef7b2b82d324
asdasd950 <o60417o@yahoo.com.tw>
2020-04-11 21:55:23 +0800

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,18 +1,17 @@
 
-
 public class factorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int i,j,mul,sum=0;
-	
-for(i=1;i<=10;i++) {
-	mul=1;
-	for(j=1;j<=i;j++) {
-		mul=mul*j;
+		int i, j, mul, sum = 0;
+
+		for (i = 1; i <= 10; i++) {
+			mul = 1;
+			for (j = 1; j <= i; j++) {
+				mul = mul * j;
+			}
+			sum = sum + mul;
+		}
+		System.out.println(sum);
 	}
-	sum=sum+mul;
-}
-System.out.println(sum);
-}
 }
